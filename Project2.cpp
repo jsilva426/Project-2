9e178cd4ccf8306dec440ddb6a37ba0b33b86c0e
jsilva426 <43155491+jsilva426@users.noreply.github.com>
2018-12-15 10:26:50 -0700

    Update Project2.cpp

--- Project2.cpp
+++ Project2.cpp
@@ -11,11 +11,47 @@ using std::vector;
 
 int main()
 {
-vector<struct node> tree;
-rootNode(tree, "this is a test");
+	string input, par, id, event;
+	vector<struct node> tree;
+	
+	cout << " Tracking Tree" << endl << "----------------------------------" << endl;
+	cout << " Instructions:CREATE ROOT FIRST|root(creates root of tree)| new (for new node)| preOrder(Displays preOrder traversal)," << endl;
+	cout << "search - display(shows inormation for a specific node)| update(update contents of specific node)| DONE (closes application)" << endl;
+	cout << "----------------------------------" << endl;
+	cin >> input;
+	if (input == "root")
+	{
+		cout << "Enter your raw event: ";
+		cin >> event;
+		rootNode(tree, event);
+		cout << endl << "Root Created" << endl;
+	}
+	if (input == "new")
+	{
+		cout << "Enter your raw event: ";
+		cin >> event;
+		insert(tree, event);
+		cout << endl << "Node sucsessfully added" << endl;
 
-cout << tree[0].ID;
-    
+	}
+	if (input == "preorder")
+	{
+		cout << "Enter your raw event: ";
+		cin >> event;
+		insert(tree, event);
+		cout << endl << "Node sucsessfully added" << endl;
+	}
+	if (input == "search")
+	{
+
+	}if (input == "update")
+	{
+
+	}
+	if (input == "DONE")
+	{
+
+	}
 return 0;
 }
 
